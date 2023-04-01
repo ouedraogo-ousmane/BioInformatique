c8f3a96ab17101ad5ff0ae79630a110d4ffb34c3
ouedraogo salam <salamouedraogo245@gmail.com>
2023-04-01 13:59:26 +0000

    corrige : ajout de la fonctionnalité de verification des séquences saisies par l'utilisateur

--- application/urls.py
+++ application/urls.py
@@ -5,15 +5,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('linear-gap/', views.global_view, name='linear'),
-    path('affine-gap/', views.global_view, name='affine'),
+    path('linear-gap/', views.global_alignGap_lineaire_view, name='linear'),
+    path('affine-gap/', views.global_alignGap_Affine_view, name='affine'),
     path('global/', views.global_view, name='global'),
     path('local/', views.local_view, name='local'),
     path('exacte/', views.needleman_wunsch_view, name='exacte'),
     path('clustal/', views.upload_file, name='clustal'),
     path('arbre/', views.dnd_view, name='arbre'),
-    path('globalGapAffine/', views.global_alignGap_Affine_view,
-         name='globalGapAffine'),
-    path('globalGapLineaire/', views.global_alignGap_lineaire_view,
-         name='globalGapLineaire'),
+    # path('globalGapAffine/', views.global_alignGap_Affine_view,
+    #      name='globalGapAffine'),
+    # path('globalGapLineaire/', views.global_alignGap_lineaire_view,
+    #      name='globalGapLineaire'),
 ]
