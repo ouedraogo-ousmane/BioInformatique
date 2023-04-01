f48cffc46382bb6792186617cc191394ea231f55
ouedraogo salam <salamouedraogo245@gmail.com>
2023-04-01 10:13:50 +0000

    feat : ajout de la barre de navigation v1

--- application/urls.py
+++ application/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import (majuscule_view,global_view,local_view,
-needleman_wunsch_view,global_alignGap_view,clustal_Align_view)
+from . import views
+# (majuscule_view,global_view,local_view,
+# needleman_wunsch_view,global_alignGap_view,clustal_Align_view)
 
 urlpatterns = [
-    path('majuscule/', majuscule_view, name='majuscule'),
-    path('global/', global_view, name='global'),
-    path('local/', local_view, name='local'),
-    path('exacte/', needleman_wunsch_view, name='exacte'),
-    path('globalGap/', global_alignGap_view, name='globalGap'),
-    path('clustal/', clustal_Align_view, name='clustal'),
+    path('', views.home_view, name='home'),
+    path('majuscule/', views.majuscule_view, name='majuscule'),
+    path('global/', views.global_view, name='global'),
+    path('local/', views.local_view, name='local'),
+    path('exacte/', views.needleman_wunsch_view, name='exacte'),
+    path('globalGap/', views.global_alignGap_view, name='globalGap'),
+    path('clustal/', views.clustal_Align_view, name='clustal'),
 ]
--- application/views.py
+++ application/views.py
@@ -2,18 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import numpy as np
 from Bio.Seq import Seq
-from Bio import pairwise2,SeqIO
+from Bio import pairwise2, SeqIO
 from Bio.Align.Applications import ClustalwCommandline
 import os
 from django.conf import settings
 # Create your views here.
+
+
+def home_view(request):
+    return render(request, 'index.html')
+
+
 def majuscule_view(request):
     if request.method == 'POST':
         chaine = request.POST.get('chaine', '')
         sequence = request.POST.get('sequence', '')
         seq1 = Seq(chaine)
         seq2 = Seq(sequence)
-        alignement = pairwise2.align.globalxx(seq1,seq2)
+        alignement = pairwise2.align.globalxx(seq1, seq2)
         return render(request, 'majuscule.html', {'chaine_maj': alignement, 'chaine': seq2.complement()})
     else:
         return render(request, 'form.html')
@@ -21,16 +27,19 @@ def majuscule_view(request):
     """Cette methode permet de determiner l'alignement globale ==
     de deux sequences seq1 et seq2
     """
+
+
 def global_view(request):
     if request.method == 'POST':
         chaine = request.POST.get('chaine', '')
         sequence = request.POST.get('sequence', '')
         seq1 = Seq(chaine)
         seq2 = Seq(sequence)
-        alignement = pairwise2.align.globalxx(seq1,seq2)
-        return render(request, 'global.html', {'seq1': seq1, 'seq2': seq2,'resultat':alignement})
+        alignement = pairwise2.align.globalxx(seq1, seq2)
+        return render(request, 'global.html', {'seq1': seq1, 'seq2': seq2, 'resultat': alignement})
     else:
         return render(request, 'form.html')
+
 
 def local_view(request):
     if request.method == 'POST':
@@ -38,24 +47,25 @@ def local_view(request):
         sequence = request.POST.get('sequence', '')
         seq1 = Seq(chaine)
         seq2 = Seq(sequence)
-        alignement = pairwise2.align.localxx(seq1,seq2)
-        return render(request, 'local.html', {'seq1': seq1, 'seq2': seq2,'resultat':alignement})
+        alignement = pairwise2.align.localxx(seq1, seq2)
+        return render(request, 'local.html', {'seq1': seq1, 'seq2': seq2, 'resultat': alignement})
     else:
         return render(request, 'form.html')
 
 
 def global_alignGap_view(request):
-    gap_open=-2
+    gap_open = -2
     gap_closed = -0.5
-    match_score=1
-    mismatch_penalty=-1
+    match_score = 1
+    mismatch_penalty = -1
     if request.method == 'POST':
         chaine = request.POST.get('chaine', '')
         sequence = request.POST.get('sequence', '')
         seq1 = Seq(chaine)
         seq2 = Seq(sequence)
-        alignement = pairwise2.align.globalms(seq1,seq2,match_score,mismatch_penalty,gap_open,gap_closed)
-        return render(request, 'globalGap.html', {'seq1': seq1, 'seq2': seq2,'resultat':alignement})
+        alignement = pairwise2.align.globalms(
+            seq1, seq2, match_score, mismatch_penalty, gap_open, gap_closed)
+        return render(request, 'globalGap.html', {'seq1': seq1, 'seq2': seq2, 'resultat': alignement})
     else:
         return render(request, 'form.html')
 
@@ -70,18 +80,20 @@ def clustal_Align_view(request):
         clustalw_cline = ClustalwCommandline("clustalw", infile=chemin_abs)
         #stdout, stderr = clustalw_cline()
         alignement = AlignIO.read("fichier.aln", "clustal")
-        return render(request, 'clustal.html', {'sequences':sequences,'resultat':alignement})
+        return render(request, 'clustal.html', {'sequences': sequences, 'resultat': alignement})
     else:
         return render(request, 'form1.html')
+
 
 def arbre_WPGMA(request):
     pass
 
+
 def needleman_wunsch_view(request):
 
-    gap_penalty=-1
-    match_score=1
-    mismatch_penalty=-1
+    gap_penalty = -1
+    match_score = 1
+    mismatch_penalty = -1
     if request.method == 'POST':
         chaine = request.POST.get('chaine', '')
         sequence = request.POST.get('sequence', '')
@@ -90,26 +102,27 @@ def needleman_wunsch_view(request):
         # Création de la matrice de scores
         m, n = len(s1), len(s2)
         score_matrix = np.zeros((m+1, n+1))
-        
+
         for i in range(1, m+1):
             score_matrix[i][0] = score_matrix[i-1][0] + gap_penalty
-            
+
         for j in range(1, n+1):
             score_matrix[0][j] = score_matrix[0][j-1] + gap_penalty
-            
+
         for i in range(1, m+1):
             for j in range(1, n+1):
                 if s1[i-1] == s2[j-1]:
                     score_matrix[i][j] = score_matrix[i-1][j-1] + match_score
                 else:
                     score_matrix[i][j] = max(score_matrix[i-1][j] + gap_penalty,
-                                            score_matrix[i][j-1] + gap_penalty,
-                                            score_matrix[i-1][j-1] + mismatch_penalty)
-        
+                                             score_matrix[i][j-1] +
+                                             gap_penalty,
+                                             score_matrix[i-1][j-1] + mismatch_penalty)
+
         # Récupération de l'alignement optimal
         align1, align2 = '', ''
         i, j = m, n
-        
+
         while i > 0 and j > 0:
             if score_matrix[i][j] == score_matrix[i-1][j-1] + (match_score if s1[i-1] == s2[j-1] else mismatch_penalty):
                 align1 = s1[i-1] + align1
@@ -124,21 +137,18 @@ def needleman_wunsch_view(request):
                 align1 = '-' + align1
                 align2 = s2[j-1] + align2
                 j -= 1
-        
+
         while i > 0:
             align1 = s1[i-1] + align1
             align2 = '-' + align2
             i -= 1
-        
+
         while j > 0:
             align1 = '-' + align1
             align2 = s2[j-1] + align2
             j -= 1
-        
+
         # return align1, align2
-        return render(request, 'exact.html', {'seq1': s1, 'seq2': s1,'alignement1':align1,'alignement2':align2})
+        return render(request, 'exact.html', {'seq1': s1, 'seq2': s1, 'alignement1': align1, 'alignement2': align2})
     else:
         return render(request, 'form.html')
-
-
-
