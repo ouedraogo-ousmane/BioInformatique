e6842cc9616ca48fb2a98b546c4a4803057c755c
OUEDRAOGO Ousmane <generaloks496@gmail.com>
2023-04-01 12:49:37 +0000

    Gap lineaire view updated

--- application/urls.py
+++ application/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import (global_view,local_view,
-needleman_wunsch_view,global_alignGap_view,clustal_Align_view,dnd_view,upload_file,arbre_WPGMA)
+needleman_wunsch_view,multiple_align,global_alignGap_Affine_view,clustal_Align_view,global_alignGap_lineaire_view,dnd_view,upload_file,arbre_WPGMA)
 
 urlpatterns = [
     path('global/', global_view, name='global'),
     path('local/', local_view, name='local'),
+    path('multiple/', multiple_align, name='multiple'),
     path('exacte/', needleman_wunsch_view, name='exacte'),
-    path('globalGap/', global_alignGap_view, name='globalGap'),
+    path('globalGapAffine/', global_alignGap_Affine_view, name='globalGapAffine'),
+    path('globalGapLineaire/', global_alignGap_lineaire_view, name='globalGapLineaire'),
     path('clustal/', upload_file, name='clustal'),
     path('arbre/', dnd_view, name='arbre'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
